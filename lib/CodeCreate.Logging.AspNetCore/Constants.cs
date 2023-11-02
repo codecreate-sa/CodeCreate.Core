41d81325c4a2f5e8e9f87f3cf8d155293af2a4c3
Orestis Meikopoulos <diktyas1988@gmail.com>
2023-11-02 12:00:05 +0200

    Added the CodeCreate.Logging.AspNetCore library.

---
+++ lib/CodeCreate.Logging.AspNetCore/Constants.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeCreate.Logging.AspNetCore
+{
+    /// <summary>
+    /// The CodeCreate.Logging.AspNetCore library constants
+    /// </summary>
+    public static class Constants
+    {
+        /// <summary>
+        /// The CorrelationId constants
+        /// </summary>
+        public static class CorrelationId
+        {
+            /// <summary>
+            /// The DefaultHeader constant
+            /// </summary>
+            public const string DefaultHeader = "x-correlation-id";
+        }
+    }
+}
---
+++ lib/CodeCreate.Logging.AspNetCore/CorrelationIdMiddleware.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace CodeCreate.Logging.AspNetCore
+{
+    /// <summary>
+    /// The middleware for fetching the correlation id
+    /// </summary>
+    public class CorrelationIdMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        /// <summary>
+        /// The CorrelationIdMiddleware constructor
+        /// </summary>
+        /// <param name="next"></param>
+        public CorrelationIdMiddleware(RequestDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        /// <summary>
+        /// The Invoke method
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="provider"></param>
+        /// <returns></returns>
+        public async Task Invoke(HttpContext context, ICorrelationIdProvider provider)
+        {
+            var correlationId = CreateCorrelationId(context);
+            provider.SetCorrelationId(correlationId);
+
+            context.TraceIdentifier = correlationId;
+
+            context.Response.OnStarting(() => 
+            {
+                if (!context.Response.Headers.ContainsKey(Constants.CorrelationId.DefaultHeader)) 
+                {
+                    context.Response.Headers.Add(
+                        Constants.CorrelationId.DefaultHeader, 
+                        correlationId
+                    );
+                }
+
+                return Task.CompletedTask;
+            });
+
+            await _next(context);
+        }
+
+        private static string CreateCorrelationId(HttpContext context)
+        {
+            var headerValue = context?
+                .Request?
+                .Headers[Constants.CorrelationId.DefaultHeader];
+
+            return !string.IsNullOrWhiteSpace(headerValue) ? 
+                headerValue :
+                $"{Guid.NewGuid()}";
+        }
+    }
+}
