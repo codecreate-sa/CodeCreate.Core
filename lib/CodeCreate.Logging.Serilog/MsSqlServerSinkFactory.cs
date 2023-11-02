4106789ef3c3e43a842f06ec854eae366e800f06
Orestis Meikopoulos <diktyas1988@gmail.com>
2023-11-02 11:50:29 +0200

    Add the CodeCreate.Logging.Serilog class library

---
+++ lib/CodeCreate.Logging.Serilog/MsSqlServerSinkFactory.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Serilog.Events;
+using Serilog.Sinks.MSSqlServer;
+using Serilog;
+
+namespace CodeCreate.Logging.Serilog
+{
+    /// <summary>
+    /// The MsSqlServerSinkFactory class
+    /// </summary>
+    public static class MsSqlServerSinkFactory
+    {
+        /// <summary>
+        /// The CreateDefaultDatabaseLogger method
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        public static LoggerConfiguration CreateDefaultDatabaseLogger(string connectionString)
+        {
+            if (connectionString == null) 
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+
+            var sinkOptions = new MSSqlServerSinkOptions 
+            {
+                TableName = Constants.MSSqlServerSink.TableName,
+                SchemaName = Constants.MSSqlServerSink.SchemaName,
+                BatchPostingLimit = Constants.MSSqlServerSink.BatchPostingLimit
+            };
+
+            var columnOpts = new ColumnOptions();
+            columnOpts.TimeStamp.DataType = SqlDbType.DateTimeOffset;
+
+            // LogEvent is in JSON format
+            columnOpts.Store.Add(StandardColumn.LogEvent);
+            columnOpts.Store.Remove(StandardColumn.Properties);
+            columnOpts.Store.Remove(StandardColumn.MessageTemplate);
+
+            columnOpts.AdditionalColumns = new List<SqlColumn>
+            {
+                new SqlColumn
+                {
+                    AllowNull = true,
+                    ColumnName = Constants.SqlColumnName.CorrelationId,
+                    DataType = SqlDbType.NVarChar
+                },
+                new SqlColumn
+                {
+                    AllowNull = true,
+                    ColumnName = Constants.SqlColumnName.AppEventId,
+                    DataType = SqlDbType.Int                    
+                }
+            };
+
+            return new LoggerConfiguration()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                .Enrich.FromLogContext()
+                .WriteTo.MSSqlServer(
+                    connectionString: connectionString,
+                    sinkOptions: sinkOptions,
+                    columnOptions: columnOpts
+                );
+        }
+    }
+}
---
+++ lib/CodeCreate.Logging.Serilog/SerilogLoggerService.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using Serilog;
+
+namespace CodeCreate.Logging.Serilog
+{
+    /// <summary>
+    /// The SerilogLoggerService implementation of the ILoggerService interface
+    /// </summary>
+    public class SerilogLoggerService : ILoggerService
+    {
+        private readonly ICorrelationIdProvider _correlationIdProvider;
+
+        private const string LogMessage = Constants.LogMessageTemplate;
+
+        /// <summary>
+        /// The SerilogLoggerService constructor
+        /// </summary>
+        /// <param name="correlationIdProvider"></param>
+        public SerilogLoggerService(ICorrelationIdProvider correlationIdProvider)
+        {
+            _correlationIdProvider = correlationIdProvider;
+        }
+
+        /// <summary>
+        /// An overload of the LogError method
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
+        public LogEntry LogError(string message)
+        {
+            return LogErrorInternal(null, message, null, null);
+        }
+
+        /// <summary>
+        /// An overload of the LogError method
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public LogEntry LogError(string message, object data)
+        {
+            return LogErrorInternal(null, message, null, data);
+        }
+
+        /// <summary>
+        /// An overload of the LogError method
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="appEventId"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public LogEntry LogError(string message, int appEventId, object? data = null)
+        {
+            return LogErrorInternal(null, message, appEventId, data);
+        }
+
+        /// <summary>
+        /// An overload of the LogError method
+        /// </summary>
+        /// <param name="ex"></param>
+        /// <param name="message"></param>
+        /// <param name="appEventId"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public LogEntry LogError(Exception ex, string message, int? appEventId = null, object? data = null)
+        {
+            return LogErrorInternal(ex, message, appEventId, data);
+        }
+
+        /// <summary>
+        /// An overload of the LogInformation method
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public LogEntry LogInformation(string message, object? data = null)
+        {
+            return LogInformationInternal(message, null, data);
+        }
+
+        /// <summary>
+        /// An overload of the LogInformation method
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="appEventId"></param>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public LogEntry LogInformation(string message, int appEventId, object? data = null)
+        {
+            return LogInformationInternal(message, appEventId, data);
+        }
+
+        private LogEntry LogErrorInternal(Exception? ex, string message, int? appEventId, object? data)
+        {
+            var correlationId = _correlationIdProvider.GetCorrelationId();
+
+            if (ex == null) 
+            {
+                Log.Error(
+                    LogMessage,
+                    message, 
+                    data, 
+                    appEventId, 
+                    correlationId
+                );
+            } 
+            else 
+            {
+                Log.Error(
+                    ex, 
+                    LogMessage,
+                    message, 
+                    data, 
+                    appEventId, 
+                    correlationId
+                );
+            }
+
+            return new LogEntry 
+            {
+                Message = message,
+                AppEventId = appEventId,
+                CorrelationId = correlationId
+            };
+        }
+
+        private LogEntry LogInformationInternal(string message, int? appEventId, object? data)
+        {
+            var correlationId = _correlationIdProvider.GetCorrelationId();
+
+            Log.Information(
+                LogMessage,
+                message, 
+                data, 
+                appEventId, 
+                correlationId
+            );
+
+            return new LogEntry 
+            {
+                Message = message,
+                AppEventId = appEventId,
+                CorrelationId = correlationId
+            };
+        }
+    }
+}
