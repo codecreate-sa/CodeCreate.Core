51f7908e53c7d2ea4722d11f1c9b7c7c422e6c56
Odysseas Vangelis <dev@odyvangelis.com>
2024-07-08 11:20:58 +0300

    expose topic and subscriptionid

--- lib/CodeCreate.Core/Messaging/IMessageBus.cs
+++ lib/CodeCreate.Core/Messaging/IMessageBus.cs
@@ -22,10 +22,32 @@ namespace CodeCreate.Messaging
         /// 
         /// </summary>
         /// <param name="message"></param>
+        /// <param name="topic"></param>
+        /// <param name="delay"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        bool Publish<T>(T message, string topic, TimeSpan? delay = null)
+            where T : class;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
         /// <param name="delay"></param>
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         Task<bool> PublishAsync<T>(T message, TimeSpan? delay = null)
+            where T : class;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="topic"></param>
+        /// <param name="delay"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        Task<bool> PublishAsync<T>(T message, string topic, TimeSpan? delay = null)
             where T : class;
 
         /// <summary>
@@ -35,6 +57,27 @@ namespace CodeCreate.Messaging
         /// <typeparam name="T"></typeparam>
         /// <returns></returns>
         IDisposable Subscribe<T>(Func<T, Task> onMessage)
+            where T : class;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="subscriptionId"></param>
+        /// <param name="onMessage"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        IDisposable Subscribe<T>(string subscriptionId, Func<T, Task> onMessage)
+            where T : class;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="subscriptionId"></param>
+        /// <param name="topic"></param>
+        /// <param name="onMessage"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        IDisposable Subscribe<T>(string subscriptionId, string topic, Func<T, Task> onMessage)
             where T : class;
     }
 }
--- lib/CodeCreate.Messaging/RabbitMqMessageBus.cs
+++ lib/CodeCreate.Messaging/RabbitMqMessageBus.cs
@@ -54,6 +54,40 @@ namespace CodeCreate.Messaging
         }
 
         /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="topic"></param>
+        /// <param name="delay"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public bool Publish<T>(T message, string topic, TimeSpan? delay = null)
+            where T : class
+        {
+            ArgumentNullException.ThrowIfNull(message);
+            ArgumentException.ThrowIfNullOrWhiteSpace(topic);
+
+            try
+            {
+                if (delay is null)
+                {
+                    _bus.PubSub.Publish(message, topic);
+                }
+                else
+                {
+                    _bus.Scheduler.FuturePublish(message, delay.Value, topic);
+                }
+            }
+            catch (Exception e)
+            {
+                // log?
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
         ///
         /// </summary>
         /// <param name="message"></param>
@@ -85,6 +119,32 @@ namespace CodeCreate.Messaging
             return true;
         }
 
+        public async Task<bool> PublishAsync<T>(T message, string topic, TimeSpan? delay = null)
+            where T : class
+        {
+            ArgumentNullException.ThrowIfNull(message);
+            ArgumentException.ThrowIfNullOrWhiteSpace(topic);
+
+            try
+            {
+                if (delay is null)
+                {
+                    await _bus.PubSub.PublishAsync(message, topic);
+                }
+                else
+                {
+                    await _bus.Scheduler.FuturePublishAsync(message, delay.Value, c => c.WithTopic(topic));
+                }
+            }
+            catch (Exception e)
+            {
+                // log
+                return false;
+            }
+
+            return true;
+        }
+
         /// <summary>
         ///
         /// </summary>
@@ -96,7 +156,41 @@ namespace CodeCreate.Messaging
         {
             ArgumentNullException.ThrowIfNull(onMessage);
 
-            return _bus.PubSub.Subscribe("dev", onMessage);
+            return _bus.PubSub.Subscribe("default", onMessage);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="subscriptionId"></param>
+        /// <param name="onMessage"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public IDisposable Subscribe<T>(string subscriptionId, Func<T, Task> onMessage)
+            where T : class
+        {
+            ArgumentNullException.ThrowIfNull(onMessage);
+            ArgumentException.ThrowIfNullOrWhiteSpace(subscriptionId);
+
+            return _bus.PubSub.Subscribe("default", onMessage);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="subscriptionId"></param>
+        /// <param name="topic"></param>
+        /// <param name="onMessage"></param>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public IDisposable Subscribe<T>(string subscriptionId, string topic, Func<T, Task> onMessage)
+            where T : class
+        {
+            ArgumentNullException.ThrowIfNull(onMessage);
+            ArgumentException.ThrowIfNullOrWhiteSpace(topic);
+            ArgumentException.ThrowIfNullOrWhiteSpace(subscriptionId);
+
+            return _bus.PubSub.Subscribe<T>(subscriptionId, (x, _) => onMessage(x), x => x.WithTopic(topic));
         }
     }
 }
